320c3ef538cd48686927ce6faa95ec08285a91a7
Tsubasa Hirota <ts-hirota@kddi.com>
2016-03-31 00:56:57 +0900

    Update softnas_test.go and Add dummy softnas-cmd

--- softnas_test.go
+++ softnas_test.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGraphDefinition(t *testing.T) {
-	var softnas SoftnasPlugin
+var testSessionIDHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"success" : true, "session_id" : 12345, "result" : {}}`)
+	return
+})
 
-	graphdef := softnas.GraphDefinition()
-	if len(graphdef) != 4 {
-		t.Errorf("GetTempfilename: %d should be 4", len(graphdef))
-	}
-	assert.EqualValues(t, "SoftNas StorageName", graphdef["softnas.storagename"].Label)
-	assert.EqualValues(t, "SoftNas StorageData", graphdef["softnas.storagedata"].Label)
-	assert.EqualValues(t, "SoftNas MemoryName", graphdef["softnas.memoryname"].Label)
-	assert.EqualValues(t, "SoftNas MemoryData", graphdef["softnas.memorydata"].Label)
+var testOverviewHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"success" : true, "session_id" : 12345, "result" : {"success":true,"msg":"","records":[{"storage_name":"44.8G Free\n(100.0%)","storage_data":99.99897820609},{"storage_name":"480.0K Used\n(0.0%)","storage_data":0.0010217939104395},{"memory_name":"666.7K\nCache Used\n(0.1%)","memory_data":0.064876091113447},{"memory_name":"1,002.9M\nCache Free\n(99.9%)","memory_data":99.935123908887}],"total":4}}`)
+	return
+})
+
+func TestSessionID(t *testing.T) {
+	http.HandleFunc("/sessionid", testSessionIDHandler)
+	http.HandleFunc("/overview", testOverviewHandler)
+	http.ListenAndServe(":8080", nil)
+	defer.close
+	id, err := getSoftnasSessionID("./softnas-cmd_test", "softnas", "Pass4W0rd")
+	assert.Nil(t, err)
+	assert.EqualValues(t, 12345, id)
 }
 
-func TestbyteSizeConvert(t *testing.T) {
+func TestConvert(t *testing.T) {
 	stub := []string{"1,000K", "1,000M", "1,000G", "1,000T", "1,000"}
 	for _, v := range stub {
 		stat, err := byteSizeConvert(v)
@@ -37,4 +48,17 @@ func TestbyteSizeConvert(t *testing.T) {
 			assert.EqualValues(t, 1000, stat)
 		}
 	}
+}
+
+func TestGraphDefinition(t *testing.T) {
+	var softnas SoftnasPlugin
+
+	graphdef := softnas.GraphDefinition()
+	if len(graphdef) != 4 {
+		t.Errorf("GetTempfilename: %d should be 4", len(graphdef))
+	}
+	assert.EqualValues(t, "SoftNas StorageName", graphdef["softnas.storagename"].Label)
+	assert.EqualValues(t, "SoftNas StorageData", graphdef["softnas.storagedata"].Label)
+	assert.EqualValues(t, "SoftNas MemoryName", graphdef["softnas.memoryname"].Label)
+	assert.EqualValues(t, "SoftNas MemoryData", graphdef["softnas.memorydata"].Label)
 }
